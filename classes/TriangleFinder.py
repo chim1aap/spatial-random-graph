e97297a783df180d491c6971152a5735634bb8c4
fam roordink <j.roordink@student.tue.nl>
2020-05-20 16:53:39 +0200

    implemented triangle finding/clustercoefficient computation

---
+++ classes/TriangleFinder.py
@@ -0,0 +1,15 @@
+import igraph as ig
+import numpy as np
+from classes.Generator import Generator
+
+class 
+
+if __name__ == '__main__':
+    # parameters
+    # delta = 0  # model parameter
+    # gamma = 0.5  # model parameter
+    # beta = 1  # model parameter
+    # n = 20  # amount of nodes
+    the_graph = Generator(delta=0.5, gamma=0.5, beta=1, n=20)
+
+
